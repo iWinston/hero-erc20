5d715b60aa027c550714b49a61c9bb4013883da7
Winston <iwinston@163.com>
2023-03-21 17:59:31 +0800

    init

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -5,14 +5,14 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract MyToken is Initializable, ERC20Upgradeable, OwnableUpgradeable {
+contract HeroToken is Initializable, ERC20Upgradeable, OwnableUpgradeable {
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
     function initialize() initializer public {
-        __ERC20_init("MyToken", "MTK");
+        __ERC20_init("HeroToken", "HTK");
         __Ownable_init();
     }
 
